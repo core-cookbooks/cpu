c978b368228fa9f91f510e1c5b0de0b3c2097a95
core-cookbooks <nagalakshmi.n@cloudenablers.com>
2015-04-20 18:30:24 +0530

    Initial Release

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "cpu"
+maintainer       "Cloudenablers"
+maintainer_email "nagalakshmi.n@cloudenablers.com"
+license          "Apache v2.0"
+description      "Manage CPU Governor on linux"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.2.0"
+supports         "ubuntu"
+supports         "debian"
